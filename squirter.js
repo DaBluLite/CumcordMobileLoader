878f35482ff4a4dd08812bbc5007a3f6d3d84c63
DaBluLite <asphatire21reborn@gmail.com>
2022-07-10 21:23:36 +0300

    Update squirter.js

--- squirter.js
+++ squirter.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('load', function() {
   fetch('https://raw.githubusercontent.com/Cumcord/builds/main/build.js')
     .then((response)=>response.text())
     .then((code)=>eval(code))
